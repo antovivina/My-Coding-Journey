270d3b7121a56cf533613e3ce3d2ff68c26342e2
antovivina <anto.vivina@gmail.com>
2026-07-29 23:52:43 +0530

    Time: 17 ms (5.31%), Space: 11.6 MB (89.62%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int c=0;
+        for(int i=0;i<ransomNote.size();i++){
+            for(int j=0;j<magazine.size();j++){
+                if(ransomNote[i]==magazine[j]){
+                    c++;
+                    magazine[j]='#';
+                    break;
+                }
+            }
+        }
+        if(c==ransomNote.size()){
+            return true;
+        }
+        return false;
+    }
+};
