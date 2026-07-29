a21c25f6325d3687ff03833f06187c9dc0b5b801
antovivina <anto.vivina@gmail.com>
2026-07-30 00:12:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (67.93%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        int i=0,j=0,c=0;
+        while(i<n&&j<m){
+            if(s[i]==t[j]){
+                c++;
+                i++;
+            }
+            j++;
+        }
+        if(c==n){
+            return true;
+        }
+        return false;
+    }
+};
