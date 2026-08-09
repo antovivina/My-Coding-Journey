817ce754342e297b67b51e8f0d0937769d33689b
antovivina <anto.vivina@gmail.com>
2026-08-09 06:55:48 +0530

    Time: 96 ms (10.97%), Space: 113.3 MB (16.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>freqarr;
-        for(int i=0;i<nums.size();i++){
-            freqarr[nums[i]]++;
-        }
-        for(unordered_map<int,int>::iterator i=freqarr.begin();i!=freqarr.end();i++){
-            if(i->second>=2){
-                return true;
-                break;
-            }
+        unordered_set<int>st(nums.begin(),nums.end());
+        if(st.size()!=nums.size()){
+            return true;
         }
         return false;
     }
