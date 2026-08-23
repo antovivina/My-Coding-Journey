a6f16042fbb2ab9dcd7d30f5b17b7638a72d9693
antovivina <anto.vivina@gmail.com>
2026-08-23 17:13:04 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.74%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        int ans=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]){
-                return t[i];
-            }
+            ans=ans^s[i];
         }
-        return t[t.size()-1];
+        for(int j=0;j<t.size();j++){
+            ans=ans^t[j];
+        }
+        return ans;
     }
 };
