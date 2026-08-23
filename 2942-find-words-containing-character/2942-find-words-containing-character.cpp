6cb9815988ca9be81126a6661a22a49a45e04f26
antovivina <anto.vivina@gmail.com>
2026-08-23 22:45:38 +0530

    Time: 0 ms (100%), Space: 32.9 MB (98.04%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.cpp
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int>ans;
         for(int i=0;i<words.size();i++){
-           if(count(words[i].begin(),words[i].end(),x)>0){
+           if(words[i].find(x)!=string::npos){
             ans.push_back(i);
            }
         }
