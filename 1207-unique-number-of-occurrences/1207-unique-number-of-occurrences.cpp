3a31e7ae4673856f6e6cdbd9f11e00599814c39d
antovivina <anto.vivina@gmail.com>
2026-08-24 12:30:06 +0530

    Time: 0 ms (100%), Space: 12.1 MB (46.88%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>freqarr;
+        for(int i=0;i<arr.size();i++){
+            freqarr[arr[i]]++;
+        }
+        vector<int>ans;
+        for(unordered_map<int,int>::iterator i=freqarr.begin();i!=freqarr.end();i++){
+            ans.push_back(i->second);
+        }
+        for(int i=0;i<ans.size()-1;i++){
+            for(int j=i+1;j<ans.size();j++){
+                if(ans[i]==ans[j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
