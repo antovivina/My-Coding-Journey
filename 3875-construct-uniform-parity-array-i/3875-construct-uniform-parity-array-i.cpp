2ef179e4ad51a86e51439b01eb9d960fb63deb79
antovivina <anto.vivina@gmail.com>
2026-09-02 19:40:58 +0530

    Time: 3 ms (5.87%), Space: 30.2 MB (94.95%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        vector<int>nums2;
-        for(int i=0;i<nums1.size();i++){
-            nums2.push_back(nums1[i]);
-        }
-        if(nums1.size()==nums2.size()){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
