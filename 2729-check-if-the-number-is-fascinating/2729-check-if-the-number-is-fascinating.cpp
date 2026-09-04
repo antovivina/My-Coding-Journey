e2ebe96e454c8570971646700d5cad6e559a8988
antovivina <anto.vivina@gmail.com>
2026-09-04 14:44:01 +0530

    Time: 0 ms (100%), Space: 9 MB (52.98%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
     bool isFascinating(int n) {
-        int m=n;
-        vector<int>ans;
-        while(n!=0){
-            int rem=n%10;
-            ans.push_back(rem);
-            n/=10;
-        }
-        int f1=m*2;
-        int f2=m*3;
-        while(f1!=0){
-            int ren=f1%10;
-            ans.push_back(ren);
-            f1/=10;
-        }
-        while(f2!=0){
-            int rek=f2%10;
-            ans.push_back(rek);
-            f2/=10;
-        }
-        if(ans.size()!=9){
+        string s=to_string(n)+to_string(n*2)+to_string(n*3);
+        if(s.size()!=9){
             return false;
         }
-        sort(ans.begin(),ans.end());
-        for(int i=0;i<9;i++){
-            if(ans[i]!=i+1){
-                return false;
-            }
+        sort(s.begin(),s.end());
+        if(s=="123456789"){
+            return true;
         }
-        return true;
+        return false;
     }
 };
