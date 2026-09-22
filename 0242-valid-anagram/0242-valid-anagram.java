39217e2fd02bcd99a456de8cbdc0c3a41813b565
antovivina <anto.vivina@gmail.com>
2026-09-22 08:48:51 +0530

    Time: 4 ms (83.76%), Space: 45.8 MB (49.82%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] arr = s.toCharArray();
+        char[] arr1 = t.toCharArray();
+        Arrays.sort(arr);
+        Arrays.sort(arr1);
+        if(s.length()==t.length()&&Arrays.equals(arr,arr1)){
+            return true;
+        }
+        return false;
+    }
+}
