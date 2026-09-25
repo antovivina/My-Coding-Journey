61387be853207f9cdac896ee3fce06e914b81df9
antovivina <anto.vivina@gmail.com>
2026-09-25 11:31:56 +0530

    Time: 10 ms (18.21%), Space: 47.1 MB (7.74%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        String ans[]=s.trim().split("\\s+");
+        String res="";
+        for(int i=ans.length-1;i>=0;i--){
+           res+=ans[i];
+           if(i!=0){
+            res+=" ";
+           }
+        }
+        return res;  
+    }
+}
